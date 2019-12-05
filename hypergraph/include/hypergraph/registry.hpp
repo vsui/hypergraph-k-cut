6fe9c4056606f654538b3798934573abeb8e01bc
Victor Sui <victorsui11@gmail.com>
2019-12-04 19:03:42 -0600

    Change naming to fix compiler error

--- hypergraph/include/hypergraph/registry.hpp
+++ hypergraph/include/hypergraph/registry.hpp
@@ -14,17 +14,17 @@ constexpr uint64_t kTestNumRuns = 0; // Use default number of runs
  */
 template<typename HypergraphType, bool Verbose = false>
 struct HypergraphMinimumCutRegistry {
-  using MinimumCutFunction =  MinimumCutFunction<HypergraphType>;
-  using MinimumKCutFunction = MinimumKCutFunction<HypergraphType>;
-  using MinimumCutFunctionWithVertex = MinimumCutFunctionWithVertex<HypergraphType>;
+  using MinCutFunction =  MinimumCutFunction<HypergraphType>;
+  using MinKCutFunction = MinimumKCutFunction<HypergraphType>;
+  using MinCutFunctionWithVertex = MinimumCutFunctionWithVertex<HypergraphType>;
 
-  const std::map<std::string, MinimumKCutFunction> minimum_k_cut_functions =
+  const std::map<std::string, MinKCutFunction> minimum_k_cut_functions =
       {
           {"CXY", cxy_k_cut_},
           {"FPZ", fpz_k_cut_}
       };
 
-  const std::map<std::string, MinimumCutFunction> minimum_cut_functions = {
+  const std::map<std::string, MinCutFunction> minimum_cut_functions = {
       {"CXY", cxy_minimum_cut_},
       {"FPZ", fpz_minimum_cut_},
       {"KW", vertex_ordering_mincut<HypergraphType, maximum_adjacency_ordering>},
@@ -32,7 +32,7 @@ struct HypergraphMinimumCutRegistry {
       {"Q", vertex_ordering_mincut<HypergraphType, queyranne_ordering>},
   };
 
-  const std::map<std::string, MinimumCutFunctionWithVertex> minimum_cut_functions_with_vertex =
+  const std::map<std::string, MinCutFunctionWithVertex> minimum_cut_functions_with_vertex =
       {
           {"KW", vertex_ordering_minimum_cut_start_vertex<HypergraphType, maximum_adjacency_ordering>},
           {"MW", vertex_ordering_minimum_cut_start_vertex<HypergraphType, tight_ordering>},
