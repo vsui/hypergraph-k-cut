0b38a186971aaa3872f2be32ca3cdd50acee6d73
Victor Sui <victorsui11@gmail.com>
2020-02-28 12:31:25 -0600

    Extract visitor from function body

--- experiment/evaluator.cpp
+++ experiment/evaluator.cpp
@@ -19,8 +19,31 @@ namespace {
 std::string hostname() {
   std::vector<char> hostname(50, '0');
   gethostname(hostname.data(), 49);
-  return {hostname};
+  return {hostname.data()};
 }
+
+// Visitor for discovery algorithms
+struct DiscoverVisitor {
+  using F = std::function<HypergraphCut<size_t>(const Hypergraph &, size_t, size_t, uint64_t)>;
+  using WF = std::function<HypergraphCut<size_t>(const WeightedHypergraph<size_t> &, size_t, size_t, uint64_t)>;
+
+  DiscoverVisitor(size_t k, size_t discovery_val, uint64_t seed, F f, WF wf)
+      : k(k), discovery_val(discovery_val), seed(seed), f(f), wf(wf) {}
+
+  size_t k;
+  size_t discovery_val;
+  uint64_t seed;
+  std::function<HypergraphCut<size_t>(const Hypergraph &, size_t, size_t, uint64_t)> f;
+  std::function<HypergraphCut<size_t>(const WeightedHypergraph<size_t> &, size_t, size_t, uint64_t)> wf;
+
+  HypergraphCut<size_t> operator()(const Hypergraph &h) {
+    return f(h, k, discovery_val, seed);
+  }
+
+  HypergraphCut<size_t> operator()(const WeightedHypergraph<size_t> &h) {
+    return wf(h, k, discovery_val, seed);
+  }
+};
 
 }
 
@@ -86,7 +109,7 @@ void MinimumCutFinder::evaluate() {
   run_info.commit = "n/a";
   run_info.info = info;
 
-  const auto [status, cut_id] = store_->report(h.name, info);
+  const auto[status, cut_id] = store_->report(h.name, info);
   if (status == ReportStatus::ERROR) {
     std::cerr << "Error when reporting cut" << std::endl;
     return;
@@ -140,32 +163,11 @@ void KDiscoveryRunner::run() {
     }
     }
 
-    // This could be probably done better
-    struct DiscoverVisitor {
-      using F = std::function<HypergraphCut<size_t>(const Hypergraph &, size_t, size_t, uint64_t)>;
-      using WF = std::function<HypergraphCut<size_t>(const WeightedHypergraph <size_t> &, size_t, size_t, uint64_t)>;
-
-      DiscoverVisitor(size_t k, size_t discovery_val, uint64_t seed, F f, WF wf)
-          : k(k), discovery_val(discovery_val), seed(seed), f(f), wf(wf) {}
-
-      size_t k;
-      size_t discovery_val;
-      uint64_t seed;
-      std::function<HypergraphCut<size_t>(const Hypergraph &, size_t, size_t, uint64_t)> f;
-      std::function<HypergraphCut<size_t>(const WeightedHypergraph <size_t> &, size_t, size_t, uint64_t)> wf;
-
-      HypergraphCut<size_t> operator()(const Hypergraph &h) {
-        return f(h, k, discovery_val, seed);
-      }
-
-      HypergraphCut<size_t> operator()(const WeightedHypergraph <size_t> &h) {
-        return wf(h, k, discovery_val, seed);
-      }
-    } cxy_visit
-        (cut.k, cut.cut_value, 1337, cxy::discover<Hypergraph, 1>, cxy::discover<WeightedHypergraph < size_t>, 1 > ),
-        fpz_visit
-        (cut.k, cut.cut_value, 1337, fpz::discover<Hypergraph, 1>, fpz::discover<WeightedHypergraph < size_t>, 1 > );
     // TODO Random seed
+    DiscoverVisitor cxy_visit
+        (cut.k, cut.cut_value, 1337, cxy::discover<Hypergraph, 1>, cxy::discover<WeightedHypergraph<size_t>, 1>);
+    DiscoverVisitor fpz_visit
+        (cut.k, cut.cut_value, 1337, fpz::discover<Hypergraph, 1>, fpz::discover<WeightedHypergraph<size_t>, 1>);
 
     // Now take the times
     auto start = std::chrono::high_resolution_clock::now();
