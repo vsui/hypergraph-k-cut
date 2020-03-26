b013e454dc2e38fe0e7c415b47dbfe8a075ef92e
Victor Sui <victorsui11@gmail.com>
2020-03-26 18:29:35 -0500

    Remove dead code

--- app/hexperiment/evaluator.hpp
+++ app/hexperiment/evaluator.hpp
@@ -13,34 +13,6 @@ class CutInfoStore;
 class HypergraphSource;
 class HypergraphGenerator;
 
-class CutEvaluator {
-public:
-  CutEvaluator(std::unique_ptr<HypergraphSource> &&source, std::unique_ptr<CutInfoStore> &&store);
-
-  /**
-   * Evaluate cuts.
-   */
-  virtual void run() = 0;
-
-  /**
-   * Evaluate a single cut and send any relevant data to the data store.
-   */
-  virtual void evaluate() = 0;
-
-  virtual ~CutEvaluator() = default;
-
-protected:
-  std::unique_ptr<CutInfoStore> store_;
-  std::unique_ptr<HypergraphSource> source_;
-};
-
-class MinimumCutFinder : public CutEvaluator {
-public:
-  using CutEvaluator::CutEvaluator;
-  void run() override;
-  void evaluate() override;
-};
-
 class KDiscoveryRunner {
 public:
   KDiscoveryRunner(const std::string &id,
