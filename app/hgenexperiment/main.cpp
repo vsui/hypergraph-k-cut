e9a50bc3d3a062c6f6d77b3c26fd167ff6e74588
Victor Sui <victorsui11@gmail.com>
2020-04-10 04:08:25 +0000

    Better logs

--- app/hgenexperiment/main.cpp
+++ app/hgenexperiment/main.cpp
@@ -11,17 +11,18 @@
 
 int main(int argc, const char **argv) {
   size_t edge_mult = 30;
-  size_t radius = 30;
-  size_t num_seeds = 10;
-  std::vector<double> epsilons = {1, 10};
+  double radius = 30;
+  size_t num_seeds = 10000;
+  std::vector<double> epsilons = {0.1, 1., 10.};
 
   std::vector<uint64_t> seeds(num_seeds, 0);
   // std::random_device rd;
   // std::transform(seeds.begin(), seeds.end(), seeds.begin(), [&rd](auto) { return rd(); });
   std::iota(seeds.begin(), seeds.end(), 0);
 
-  for (size_t n : {100, 112, 125, 137, 150}) {
+  for (size_t n : {100, 112, 125, 137, 150, 162, 175, 187, 200, 212, 225, 237, 250}) {
     for (auto seed : seeds) {
+	    std::cout << "\r" << std::flush << "[" << seed << "/" << seeds.size() << "]" << std::flush;
       const auto gen = RandomRingConstantEdgeHypergraph(n, n * edge_mult, radius, seed);
       const auto hypergraph = std::get<0>(gen.generate());
       Hypergraph copy(hypergraph);
@@ -29,7 +30,8 @@ int main(int argc, const char **argv) {
 
       const auto skew = static_cast<double>(cut.partitions[0].size()) / hypergraph.num_vertices();
       if (skew < 0.1 || skew > 0.9) {
-        std::cout << "Skewed cut, skipping..." << std::endl;
+	// Skip skewed
+	continue;
       }
 
       std::vector<double> cut_factors;
@@ -43,12 +45,17 @@ int main(int argc, const char **argv) {
                        return cut_factor;
                      });
 
-      if (std::all_of(cut_factors.begin(), cut_factors.end(), [](auto factor) { return factor > 1.5; })) {
-        std::cout << "Interesting cut factor" << std::endl;
+      if (std::all_of(cut_factors.begin() + 1 /* ignore 1 */, cut_factors.end(), [](auto factor) { return factor > 1.5; })) {
         for (int i = 0; i < epsilons.size(); ++i) {
+	std::cout << "\r" << std::flush;
           std::cout << n << " " << seed << " " << epsilons[i] << " " << cut_factors[i] << std::endl;
         }
+	goto outofloop;
       }
+      std::cout << "\r" << std::flush;
+      std::cout.flush();
     }
+outofloop:
+    std::cout << "Done with " << n << std::endl;
   }
 }
